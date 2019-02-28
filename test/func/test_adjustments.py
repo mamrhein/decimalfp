8341010166788a8bcbc6a91e2f7160797f507869
Michael Amrhein <michael@adrhinum.de>
2019-02-28 18:49:35 +0100

    Added tests for adjustment using default rounding mode.

--- test/func/test_adjustments.py
+++ test/func/test_adjustments.py
@@ -15,7 +15,13 @@
 """Test driver for package 'decimalfp' (adjustments)."""
 
 
+from fractions import Fraction
 import pytest
+
+from decimalfp import set_rounding, ROUNDING
+
+
+set_rounding(ROUNDING.ROUND_HALF_UP)
 
 
 @pytest.mark.parametrize(("value", "prec"),
@@ -28,3 +34,16 @@ def test_normalize(impl, value, prec):
     adj = dec.adjusted()
     assert adj.precision == prec
     assert dec.as_fraction() == adj.as_fraction()
+
+
+@pytest.mark.parametrize(("value", "prec", "numerator"),
+                         (("17.849", 1, 178),
+                          (".".join(("1" * 3297, "4" * 33)), -3, int("1" * 3294 + "000")),
+                          ("0.00015", 4, 2)),
+                         ids=("compact", "large", "fraction"))
+def test_adjust_dflt_round(impl, value, prec, numerator):
+    dec = impl.Decimal(value)
+    adj = dec.adjusted(prec)
+    res_prec = max(prec, 0)
+    assert adj.precision == res_prec
+    assert adj.as_fraction() == Fraction(numerator, 10 ** res_prec)
