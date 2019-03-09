a45c60420e21646d5a4611106afc42d008885e14
Michael Amrhein <michael@adrhinum.de>
2019-03-09 17:12:06 +0100

    Added tests for adjustment using given rounding mode.

--- test/func/test_adjustments.py
+++ test/func/test_adjustments.py
@@ -15,6 +15,8 @@
 """Test driver for package 'decimalfp' (adjustments)."""
 
 
+from decimal import Decimal as StdLibDecimal
+from decimal import getcontext
 from fractions import Fraction
 import pytest
 
@@ -22,6 +24,8 @@ from decimalfp import set_rounding, ROUNDING
 
 
 set_rounding(ROUNDING.ROUND_HALF_UP)
+ctx = getcontext()
+ctx.prec = 3350
 
 
 @pytest.mark.parametrize(("value", "prec"),
@@ -38,7 +42,8 @@ def test_normalize(impl, value, prec):
 
 @pytest.mark.parametrize(("value", "prec", "numerator"),
                          (("17.849", 1, 178),
-                          (".".join(("1" * 3297, "4" * 33)), -3, int("1" * 3294 + "000")),
+                          (".".join(("1" * 3297, "4" * 33)), -3,
+                           int("1" * 3294 + "000")),
                           ("0.00015", 4, 2)),
                          ids=("compact", "large", "fraction"))
 def test_adjust_dflt_round(impl, value, prec, numerator):
@@ -47,3 +52,29 @@ def test_adjust_dflt_round(impl, value, prec, numerator):
     res_prec = max(prec, 0)
     assert adj.precision == res_prec
     assert adj.as_fraction() == Fraction(numerator, 10 ** res_prec)
+
+
+@pytest.mark.parametrize("rnd",
+                         [rnd for rnd in ROUNDING],
+                         ids=[rnd.name for rnd in ROUNDING])
+@pytest.mark.parametrize("value",
+                         ("17.849",
+                          ".".join(("1" * 3297, "4" * 33)),
+                          "0.00015"),
+                         ids=("compact",
+                              "large",
+                              "fraction"))
+@pytest.mark.parametrize("prec", (1,
+                                  -3,
+                                  4),
+                         ids=("1",
+                              "-3",
+                              "4"))
+def test_adjust_round(impl, rnd, value, prec):
+    dec = impl.Decimal(value)
+    adj = dec.adjusted(prec, rnd)
+    res_prec = max(prec, 0)
+    assert adj.precision == res_prec
+    quant = StdLibDecimal("1e%i" % -prec)
+    eq_dec = StdLibDecimal(value).quantize(quant, rnd.name)
+    assert adj.as_fraction() == Fraction(*eq_dec.as_integer_ratio())
