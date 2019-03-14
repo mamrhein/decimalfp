82f0f3166162636c6ab3b39b21d921ab296ab318
Michael Amrhein <michael@adrhinum.de>
2019-03-14 16:23:57 +0100

    Refactored tests for comparisons; added tests comparing to Integral values.

--- test/func/test_comparisons.py
+++ test/func/test_comparisons.py
@@ -31,6 +31,30 @@ ctx = getcontext()
 ctx.prec = 3350
 
 
+def chk_eq(dec, equiv):
+    assert dec == equiv
+    assert dec >= equiv
+    assert dec <= equiv
+    assert not(dec != equiv)
+    assert not(dec > equiv)
+    assert not(dec < equiv)
+    # x == y  <=> hash(x) == hash (y)
+    assert hash(dec) == hash(equiv)
+
+
+@pytest.mark.parametrize("value",
+                         ("-17",
+                          "".join(("1" * 3097, "4" * 33, "0" * 19)),
+                          "-0"),
+                         ids=("compact", "large", "zero"))
+@pytest.mark.parametrize("trail", (".000", ".", ""),
+                         ids=("trail='000'", "trail='.'", "trail=''"))
+def test_eq_integral(impl, value, trail):
+    dec = impl.Decimal(value + trail)
+    equiv = int(value)
+    chk_eq(dec, equiv)
+
+
 @pytest.mark.parametrize("rational",
                          (None, StdLibDecimal, Fraction),
                          ids=("Decimal", "StdLibDecimal", "Fraction"))
@@ -48,14 +72,44 @@ def test_eq_rational(impl, rational, value, trail1, trail2):
         rational = impl.Decimal
     dec = impl.Decimal(value + trail1)
     equiv = rational(value + trail2)
-    assert dec == equiv
-    assert dec >= equiv
-    assert dec <= equiv
-    assert not(dec != equiv)
-    assert not(dec > equiv)
-    assert not(dec < equiv)
-    # x == y  <=> hash(x) == hash (y)
-    assert hash(dec) == hash(equiv)
+    chk_eq(dec, equiv)
+
+
+def chk_gt(dec, non_equiv_gt):
+    assert dec != non_equiv_gt
+    assert dec < non_equiv_gt
+    assert dec <= non_equiv_gt
+    assert not(dec == non_equiv_gt)
+    assert not(dec > non_equiv_gt)
+    assert not(dec >= non_equiv_gt)
+    # x != y  <=> hash(x) != hash (y)
+    assert hash(dec) != hash(non_equiv_gt)
+
+
+def chk_lt(dec, non_equiv_lt):
+    assert dec != non_equiv_lt
+    assert dec > non_equiv_lt
+    assert dec >= non_equiv_lt
+    assert not(dec == non_equiv_lt)
+    assert not(dec < non_equiv_lt)
+    assert not(dec <= non_equiv_lt)
+    # x != y  <=> hash(x) != hash (y)
+    assert hash(dec) != hash(non_equiv_lt)
+
+
+@pytest.mark.parametrize("value",
+                         ("-17",
+                          "".join(("1" * 3097, "4" * 33, "0" * 19)),
+                          "-0"),
+                         ids=("compact", "large", "zero"))
+def test_ne_integral(impl, value):
+    non_equiv = int(value)
+    prec = len(value)
+    delta = Fraction(1, 10 ** prec)
+    dec_gt = impl.Decimal(non_equiv + delta, prec)
+    chk_lt(dec_gt, non_equiv)
+    dec_lt = impl.Decimal(non_equiv - delta, prec)
+    chk_gt(dec_lt, non_equiv)
 
 
 @pytest.mark.parametrize("rational",
@@ -70,23 +124,10 @@ def test_ne_rational(impl, rational, value):
     if rational is None:
         rational = impl.Decimal
     dec = impl.Decimal(value)
-    non_equiv_lt = rational(value) - rational(1) / 10 ** 180
-    assert dec != non_equiv_lt
-    assert dec > non_equiv_lt
-    assert dec >= non_equiv_lt
-    assert not(dec == non_equiv_lt)
-    assert not(dec < non_equiv_lt)
-    assert not(dec <= non_equiv_lt)
     non_equiv_gt = rational(value) + rational(1) / 10 ** 180
-    assert dec != non_equiv_gt
-    assert dec < non_equiv_gt
-    assert dec <= non_equiv_gt
-    assert not(dec == non_equiv_gt)
-    assert not(dec > non_equiv_gt)
-    assert not(dec >= non_equiv_gt)
-    # x != y  <=> hash(x) != hash (y)
-    assert hash(dec) != hash(non_equiv_lt)
-    assert hash(dec) != hash(non_equiv_gt)
+    chk_gt(dec, non_equiv_gt)
+    non_equiv_lt = rational(value) - rational(1) / 10 ** 180
+    chk_lt(dec, non_equiv_lt)
 
 
 @pytest.mark.parametrize("other", ["1/5", operator.ne],
