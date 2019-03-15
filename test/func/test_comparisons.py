d0d32806e57e7a3a7c7b6a5b908ee0638dea1506
Michael Amrhein <michael@adrhinum.de>
2019-03-15 22:19:29 +0100

    Added tests comparing to Real and Complex values.

--- test/func/test_comparisons.py
+++ test/func/test_comparisons.py
@@ -19,6 +19,7 @@ from decimal import Decimal as StdLibDecimal
 from decimal import getcontext
 from fractions import Fraction
 import operator
+import sys
 
 import pytest
 
@@ -75,6 +76,32 @@ def test_eq_rational(impl, rational, value, trail1, trail2):
     chk_eq(dec, equiv)
 
 
+@pytest.mark.parametrize("value",
+                         ("17.500",
+                          sys.float_info.max * 0.9,
+                          "%1.63f" % (1 / sys.maxsize)),
+                         ids=("compact", "large", "fraction"))
+def test_eq_real(impl, value):
+    dec = impl.Decimal(value, 400)
+    equiv = float(value)
+    chk_eq(dec, equiv)
+
+
+@pytest.mark.parametrize("value",
+                         ("17.500",
+                          sys.float_info.max,
+                          "%1.63f" % (1 / sys.maxsize)),
+                         ids=("compact", "large", "fraction"))
+def test_eq_complex(impl, value):
+    dec = impl.Decimal(value, 400)
+    equiv = complex(value)
+    non_equiv = complex(float(value), 1)
+    assert dec == equiv
+    assert not (dec != equiv)
+    assert dec != non_equiv
+    assert not (dec == non_equiv)
+
+
 def chk_gt(dec, non_equiv_gt):
     assert dec != non_equiv_gt
     assert dec < non_equiv_gt
@@ -128,6 +155,31 @@ def test_ne_rational(impl, rational, value):
     chk_gt(dec, non_equiv_gt)
     non_equiv_lt = rational(value) - rational(1) / 10 ** 180
     chk_lt(dec, non_equiv_lt)
+
+
+@pytest.mark.parametrize("value",
+                         ("17.500",
+                          sys.float_info.max * 0.9,
+                          "%1.63f" % (1 / sys.maxsize)),
+                         ids=("compact", "large", "fraction"))
+def test_ne_real(impl, value):
+    dec = impl.Decimal(value, 400)
+    non_equiv_gt = float(value) * (1. + 1. / 10 **14)
+    chk_gt(dec, non_equiv_gt)
+    non_equiv_lt = float(value) * (1. - 1. / 10 **14)
+    chk_lt(dec, non_equiv_lt)
+
+
+@pytest.mark.parametrize("op",
+                         [op for op in ORDERING_OPS],
+                         ids=[op.__name__ for op in ORDERING_OPS])
+@pytest.mark.parametrize("value", ["0.4", "1.75+3j"],
+                         ids=("other='0.4'", "1.75+3j"))
+def test_ord_ops_complex(impl, op, value):
+    dec = impl.Decimal('3.12')
+    cmplx = complex(value)
+    with pytest.raises(TypeError):
+        op(dec, cmplx)
 
 
 @pytest.mark.parametrize("other", ["1/5", operator.ne],
