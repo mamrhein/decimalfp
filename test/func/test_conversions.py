010de749e0daa991e5e4a766c79cfe8ec6b0b7f2
Michael Amrhein <michael@adrhinum.de>
2019-03-17 17:33:49 +0100

    Added test for 'as_tuple' and 'as_integer_ratio'.

--- test/func/test_conversions.py
+++ test/func/test_conversions.py
@@ -81,3 +81,23 @@ def test_to_float(impl, num, den):
     f = Fraction(num, den)
     dec = impl.Decimal(f, 250)
     assert float(f) == float(dec)
+
+
+@pytest.mark.parametrize("sign", (0, 1), ids=("+", "-"))
+@pytest.mark.parametrize("coeff", (178, 9 ** 378), ids=("small", "large"))
+@pytest.mark.parametrize("exp", (0, -54, 23), ids=("0", "neg", "pos"))
+def test_as_tuple(impl, sign, coeff, exp):
+    s = f"{'-' * sign}{coeff}e{exp}"
+    dec = impl.Decimal(s)
+    assert dec.as_tuple() == (sign, coeff * 10 ** max(0, exp), min(exp, 0))
+
+
+@pytest.mark.parametrize(("num", "den"),
+                         ((17, 1),
+                          (9 ** 394, 10 ** 247),
+                          (190, 400000)),
+                         ids=("compact", "large", "fraction"))
+def test_as_integer_ratio(impl, num, den):
+    f = Fraction(num, den)
+    dec = impl.Decimal(f, 250)
+    assert dec.as_integer_ratio() == (f.numerator, f.denominator)
