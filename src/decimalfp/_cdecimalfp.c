mamrhein/decimalfp

commit 7c222e26a747dd27b3ebba7991d94ac2d05b06d0

Floor division changed to always return an int.

src/decimalfp/_cdecimalfp.pyx
@@ -1307,7 +1307,7 @@ cdef tuple divmod1(Decimal x, object y):
             yv = (<Decimal>y)._value
         q = xv // yv
         r._value = xv - q * yv
-        return Decimal(q, r._precision), r
+        return q, r
     elif isinstance(y, Integral):
         r = Decimal(x)
         xv = x._value
@@ -1315,9 +1315,9 @@ cdef tuple divmod1(Decimal x, object y):
         yv = y * base10pow(xp)
         q = xv // yv
         r._value = xv - q * yv
-        return Decimal(q, xp), r
+        return q, r
     elif isinstance(y, _StdLibDecimal):
-        return x.__divmod__(Decimal(y))
+        return divmod1(x, Decimal(y))
     else:
         return x // y, x % y
 
@@ -1338,7 +1338,7 @@ cdef tuple divmod2(object x, Decimal y):
             yv = y._value
         q = xv // yv
         r._value = xv - q * yv
-        return Decimal(q, r._precision), r
+        return q, r
     elif isinstance(x, Integral):
         r = Decimal(y)
         yv = y._value
@@ -1346,9 +1346,9 @@ cdef tuple divmod2(object x, Decimal y):
         xv = x * base10pow(yp)
         q = xv // yv
         r._value = xv - q * yv
-        return Decimal(q, yp), r
+        return q, r
     elif isinstance(x, _StdLibDecimal):
-        return Decimal(x).__divmod__(y)
+        return divmod2(Decimal(x), y)
     else:
         return x // y, x % y
 
@@ -1358,7 +1358,7 @@ cdef object floordiv1(Decimal x, object y):
     if isinstance(y, (Decimal, Integral, _StdLibDecimal)):
         return divmod1(x, y)[0]
     else:
-        return Decimal(floor(x / y), x._precision)
+        return floor(x / y)
 
 
 cdef object floordiv2(object x, Decimal y):
@@ -1366,7 +1366,7 @@ cdef object floordiv2(object x, Decimal y):
     if isinstance(x, (Decimal, Integral, _StdLibDecimal)):
         return divmod2(x, y)[0]
     else:
-        return Decimal(floor(x / y), y._precision)
+        return floor(x / y)
 
 
 cdef object mod1(Decimal x, object y):

src/decimalfp/_pydecimalfp.py
@@ -1347,7 +1347,7 @@ def divmod1(x, y):
             yv = y._value
         q = xv // yv
         r._value = xv - q * yv
-        return Decimal(q, r._precision), r
+        return q, r
     elif isinstance(y, Integral):
         r = Decimal(x)
         xv = x._value
@@ -1355,9 +1355,9 @@ def divmod1(x, y):
         yv = y * base10pow(xp)
         q = xv // yv
         r._value = xv - q * yv
-        return Decimal(q, xp), r
+        return q, r
     elif isinstance(y, _StdLibDecimal):
-        return x.__divmod__(Decimal(y))
+        return divmod1(x, Decimal(y))
     else:
         return x // y, x % y
 
@@ -1380,7 +1380,7 @@ def divmod2(x, y):
             yv = y._value
         q = xv // yv
         r._value = xv - q * yv
-        return Decimal(q, r._precision), r
+        return q, r
     elif isinstance(x, Integral):
         r = Decimal(y)
         yv = y._value
@@ -1388,9 +1388,9 @@ def divmod2(x, y):
         xv = x * base10pow(yp)
         q = xv // yv
         r._value = xv - q * yv
-        return Decimal(q, yp), r
+        return q, r
     elif isinstance(x, _StdLibDecimal):
-        return Decimal(x).__divmod__(y)
+        return divmod2(Decimal(x), y)
     else:
         return x // y, x % y
 
@@ -1404,7 +1404,7 @@ def floordiv1(x, y):
     if isinstance(y, (Decimal, Integral, _StdLibDecimal)):
         return divmod1(x, y)[0]
     else:
-        return Decimal(floor(x / y), x._precision)
+        return floor(x / y)
 
 
 def floordiv2(x, y):
@@ -1416,7 +1416,7 @@ def floordiv2(x, y):
     if isinstance(x, (Decimal, Integral, _StdLibDecimal)):
         return divmod2(x, y)[0]
     else:
-        return Decimal(floor(x / y), y._precision)
+        return floor(x / y)
 
 
 def mod1(x, y):

commit b7534e0299b1e1842ff4353e794356939e5a6e5b

Fixed bug in modulo op caused by commit 7c222e2.

src/decimalfp/_cdecimalfp.c
@@ -21093,7 +21093,7 @@ static PyObject *__pyx_f_9decimalfp_11_cdecimalfp_mod1(struct __pyx_obj_9decimal
  *     if isinstance(y, (Decimal, Integral, _StdLibDecimal)):
  *         return divmod1(x, y)[1]             # <<<<<<<<<<<<<<
  *     else:
- *         return x - y * (x // y)
+ *         return x - y * Decimal(x // y)
  */
     __Pyx_XDECREF(__pyx_r);
     __pyx_t_1 = __pyx_f_9decimalfp_11_cdecimalfp_divmod1(__pyx_v_x, __pyx_v_y); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 1375, __pyx_L1_error)
@@ -21121,7 +21121,7 @@ static PyObject *__pyx_f_9decimalfp_11_cdecimalfp_mod1(struct __pyx_obj_9decimal
   /* "decimalfp/_cdecimalfp.pyx":1377
  *         return divmod1(x, y)[1]
  *     else:
- *         return x - y * (x // y)             # <<<<<<<<<<<<<<
+ *         return x - y * Decimal(x // y)             # <<<<<<<<<<<<<<
  * 
  * 
  */
@@ -21129,14 +21129,17 @@ static PyObject *__pyx_f_9decimalfp_11_cdecimalfp_mod1(struct __pyx_obj_9decimal
     __Pyx_XDECREF(__pyx_r);
     __pyx_t_2 = PyNumber_FloorDivide(((PyObject *)__pyx_v_x), __pyx_v_y); if (unlikely(!__pyx_t_2)) __PYX_ERR(0, 1377, __pyx_L1_error)
     __Pyx_GOTREF(__pyx_t_2);
-    __pyx_t_1 = PyNumber_Multiply(__pyx_v_y, __pyx_t_2); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 1377, __pyx_L1_error)
+    __pyx_t_1 = __Pyx_PyObject_CallOneArg(((PyObject *)__pyx_ptype_9decimalfp_11_cdecimalfp_Decimal), __pyx_t_2); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 1377, __pyx_L1_error)
     __Pyx_GOTREF(__pyx_t_1);
     __Pyx_DECREF(__pyx_t_2); __pyx_t_2 = 0;
-    __pyx_t_2 = PyNumber_Subtract(((PyObject *)__pyx_v_x), __pyx_t_1); if (unlikely(!__pyx_t_2)) __PYX_ERR(0, 1377, __pyx_L1_error)
+    __pyx_t_2 = PyNumber_Multiply(__pyx_v_y, __pyx_t_1); if (unlikely(!__pyx_t_2)) __PYX_ERR(0, 1377, __pyx_L1_error)
     __Pyx_GOTREF(__pyx_t_2);
     __Pyx_DECREF(__pyx_t_1); __pyx_t_1 = 0;
-    __pyx_r = __pyx_t_2;
-    __pyx_t_2 = 0;
+    __pyx_t_1 = PyNumber_Subtract(((PyObject *)__pyx_v_x), __pyx_t_2); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 1377, __pyx_L1_error)
+    __Pyx_GOTREF(__pyx_t_1);
+    __Pyx_DECREF(__pyx_t_2); __pyx_t_2 = 0;
+    __pyx_r = __pyx_t_1;
+    __pyx_t_1 = 0;
     goto __pyx_L0;
   }
 
@@ -21217,7 +21220,7 @@ static PyObject *__pyx_f_9decimalfp_11_cdecimalfp_mod2(PyObject *__pyx_v_x, stru
  *     if isinstance(x, (Decimal, Integral, _StdLibDecimal)):
  *         return divmod2(x, y)[1]             # <<<<<<<<<<<<<<
  *     else:
- *         return x - y * (x // y)
+ *         return x - y * Decimal(x // y)
  */
     __Pyx_XDECREF(__pyx_r);
     __pyx_t_1 = __pyx_f_9decimalfp_11_cdecimalfp_divmod2(__pyx_v_x, __pyx_v_y); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 1383, __pyx_L1_error)
@@ -21245,7 +21248,7 @@ static PyObject *__pyx_f_9decimalfp_11_cdecimalfp_mod2(PyObject *__pyx_v_x, stru
   /* "decimalfp/_cdecimalfp.pyx":1385
  *         return divmod2(x, y)[1]
  *     else:
- *         return x - y * (x // y)             # <<<<<<<<<<<<<<
+ *         return x - y * Decimal(x // y)             # <<<<<<<<<<<<<<
  * 
  * 
  */
@@ -21253,14 +21256,17 @@ static PyObject *__pyx_f_9decimalfp_11_cdecimalfp_mod2(PyObject *__pyx_v_x, stru
     __Pyx_XDECREF(__pyx_r);
     __pyx_t_2 = PyNumber_FloorDivide(__pyx_v_x, ((PyObject *)__pyx_v_y)); if (unlikely(!__pyx_t_2)) __PYX_ERR(0, 1385, __pyx_L1_error)
     __Pyx_GOTREF(__pyx_t_2);
-    __pyx_t_1 = PyNumber_Multiply(((PyObject *)__pyx_v_y), __pyx_t_2); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 1385, __pyx_L1_error)
+    __pyx_t_1 = __Pyx_PyObject_CallOneArg(((PyObject *)__pyx_ptype_9decimalfp_11_cdecimalfp_Decimal), __pyx_t_2); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 1385, __pyx_L1_error)
     __Pyx_GOTREF(__pyx_t_1);
     __Pyx_DECREF(__pyx_t_2); __pyx_t_2 = 0;
-    __pyx_t_2 = PyNumber_Subtract(__pyx_v_x, __pyx_t_1); if (unlikely(!__pyx_t_2)) __PYX_ERR(0, 1385, __pyx_L1_error)
+    __pyx_t_2 = PyNumber_Multiply(((PyObject *)__pyx_v_y), __pyx_t_1); if (unlikely(!__pyx_t_2)) __PYX_ERR(0, 1385, __pyx_L1_error)
     __Pyx_GOTREF(__pyx_t_2);
     __Pyx_DECREF(__pyx_t_1); __pyx_t_1 = 0;
-    __pyx_r = __pyx_t_2;
-    __pyx_t_2 = 0;
+    __pyx_t_1 = PyNumber_Subtract(__pyx_v_x, __pyx_t_2); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 1385, __pyx_L1_error)
+    __Pyx_GOTREF(__pyx_t_1);
+    __Pyx_DECREF(__pyx_t_2); __pyx_t_2 = 0;
+    __pyx_r = __pyx_t_1;
+    __pyx_t_1 = 0;
     goto __pyx_L0;
   }
 

src/decimalfp/_cdecimalfp.pyx
@@ -1374,7 +1374,7 @@ cdef object mod1(Decimal x, object y):
     if isinstance(y, (Decimal, Integral, _StdLibDecimal)):
         return divmod1(x, y)[1]
     else:
-        return x - y * (x // y)
+        return x - y * Decimal(x // y)
 
 
 cdef object mod2(object x, Decimal y):
@@ -1382,7 +1382,7 @@ cdef object mod2(object x, Decimal y):
     if isinstance(x, (Decimal, Integral, _StdLibDecimal)):
         return divmod2(x, y)[1]
     else:
-        return x - y * (x // y)
+        return x - y * Decimal(x // y)
 
 
 cdef object pow1(Decimal x, object y):

src/decimalfp/_pydecimalfp.py
@@ -1428,7 +1428,7 @@ def mod1(x, y):
     if isinstance(y, (Decimal, Integral, _StdLibDecimal)):
         return divmod1(x, y)[1]
     else:
-        return x - y * (x // y)
+        return x - y * Decimal(x // y)
 
 
 def mod2(x, y):
@@ -1440,7 +1440,7 @@ def mod2(x, y):
     if isinstance(x, (Decimal, Integral, _StdLibDecimal)):
         return divmod2(x, y)[1]
     else:
-        return x - y * (x // y)
+        return x - y * Decimal(x // y)
 
 
 def pow1(x, y):