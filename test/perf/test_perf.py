ff641f93e87e69da4be8927a744c656bd3703cb5
Michael Amrhein <michael@adrhinum.de>
2019-04-08 18:08:34 +0200

    Added benchmarks.

--- test/perf/test_perf.py
+++ test/perf/test_perf.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
 # Name:        test_perf
@@ -6,11 +5,10 @@
 #
 # Author:      Michael Amrhein michael@adrhinum.de)
 #
-# Copyright:   (c) 2014 ff. Michael Amrhein
-# License:     This program is free software. You can redistribute it, use it
-#              and/or modify it under the terms of the 2-clause BSD license.
-#              For license details please read the file LICENSE.TXT provided
-#              together with the source code.
+# Copyright:   (c) 2019 Michael Amrhein
+# License:     This program is part of a larger application. For license
+#              details please read the file LICENSE.TXT provided together
+#              with the application.
 # ----------------------------------------------------------------------------
 # $Source$
 # $Revision$
@@ -19,39 +17,25 @@
 """Compare performance of different implementations of Decimal."""
 
 
-# import math
-# import os
-# import sys
-from collections import namedtuple
-from importlib import import_module
-# from decimal import Decimal as StdLibDecimal
-
-import pytest
-
-# from decimalfp import ROUNDING
-# from decimalfp._pydecimalfp import Decimal as PyDecimal             # noqa
-# from decimalfp._cdecimalfp import Decimal as CDecimal               # noqa
+from decimal import getcontext
+import operator
 
 
-@pytest.fixture(scope="session",
-                params=("decimal", "decimalfp._pydecimalfp",
-                        "decimalfp._cdecimalfp"),
-                ids=("stdlib", "pydec", "cydec"))
-def impl(request):
-    """Return Decimal implementation."""
-    mod = import_module(request.param)
-    return mod
+ctx = getcontext()
+ctx.prec = 390
 
 
-StrVals = namedtuple('StrVals', "compact, small, large")
-str_vals = StrVals("+17.4",
-                   "-1234567890.12345678901234567890",
-                   "9" * 294 + "." + "183" * 81)
+def test_decimal_from_str(benchmark, impl, str_value):
+    benchmark(impl.Decimal, str_value)
 
 
-@pytest.mark.parametrize("value", str_vals, ids=str_vals._fields)
-def test_decimal_from_str(impl, benchmark, value):
-    """Decimal from string."""
-    Decimal = impl.Decimal                                      # noqa:N806
-    dec = benchmark(Decimal, value)
-    assert isinstance(dec, Decimal)
+def test_decimal_add(benchmark, dec_value):
+    benchmark(operator.add, dec_value, dec_value)
+
+
+def test_decimal_mul(benchmark, dec_value):
+    benchmark(operator.mul, dec_value, dec_value)
+
+
+def test_decimal_div(benchmark, dec_value):
+    benchmark(operator.truediv, dec_value, dec_value)
