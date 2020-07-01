fdcc5ba4e716c95d74930159a3655d698a531d49
Michael Amrhein <michael@adrhinum.de>
2020-07-01 16:13:33 +0200

    Updated setup.

--- setup.py
+++ setup.py
@@ -1,23 +1,51 @@
+# coding=utf-8
+"""Setup package 'decimalfp'."""
+
+import os
 from setuptools import setup, Extension
+
+
+LIBFPDEC_PATH = 'src/decimalfp/libfpdec'
+LIBFPDEC_SRC_FILES = sorted(f"{LIBFPDEC_PATH}/{fn}"
+                            for fn in os.listdir(path=LIBFPDEC_PATH)
+                            if fn.endswith(('.c', 'cpp')))
+
+
+class PybindIncludePath:
+    """Helper for determining the pybind11 include path."""
+
+    def __str__(self) -> str:
+        import pybind11
+        return pybind11.get_include()
+
+
+ext_modules = [
+    Extension(
+        'decimalfp._cdecimalfp',
+        ['src/decimalfp/_cdecimalfp.cpp'] + LIBFPDEC_SRC_FILES,
+        include_dirs=[LIBFPDEC_PATH, PybindIncludePath()],
+        # extra_link_args="",
+        language='c++',
+    ),
+]
+
 
 with open('README.md') as file:
     long_description = file.read()
 
 setup(
     name="decimalfp",
-    version="0.11.0",
+    version="0.11.0a1",
     author="Michael Amrhein",
     author_email="michael@adrhinum.de",
     url="https://github.com/mamrhein/decimalfp",
     description="Decimal fixed-point arithmetic",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_dir = {'': 'src'},
+    package_dir={'': 'src'},
     packages=['decimalfp'],
-    ext_modules=[Extension('decimalfp._cdecimalfp',
-                           ['src/decimalfp/_cdecimalfp.cpp'])],
+    ext_modules=ext_modules,
     python_requires=">=3.6",
-    # install_requires=requirements,
     license='BSD',
     keywords='fixed-point decimal number datatype',
     platforms='all',
@@ -30,10 +58,12 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules"
-    ]
+    ],
+    zip_safe=False,
 )
