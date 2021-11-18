c0e2dbaed4301841ce703feab7dab243454dad01
Michael Amrhein <michael@adrhinum.de>
2021-11-18 22:48:14 +0100

    Build extension module only if gcc available.

--- setup.py
+++ setup.py
@@ -6,32 +6,45 @@ import sysconfig
 
 from setuptools import Extension, setup
 
-LIBFPDEC_PATH = 'src/decimalfp/libfpdec'
-LIBFPDEC_SRC_FILES = sorted(f"{LIBFPDEC_PATH}/{fn}"
-                            for fn in os.listdir(path=LIBFPDEC_PATH)
-                            if fn.endswith(('.c',)))
 
-DEBUG = int(os.getenv("DEBUG", 0))
-cflags = sysconfig.get_config_var('CFLAGS')
-if cflags:
-    extra_compile_args = cflags.split() + ["-Wall", "-Wextra"]
-else:
-    extra_compile_args = ["-Wall", "-Wextra"]
-if DEBUG:
-    extra_compile_args += ["-g3", "-O0", f"-DDEBUG={DEBUG}", "-UNDEBUG"]
-else:
-    extra_compile_args += ["-DNDEBUG", "-O3"]
+def have_gcc() -> bool:
+    """Return true if gcc is available, false otherwise."""
+    with os.popen("gcc --version") as pipe:
+        rsp = pipe.read()
+    return rsp != ""
 
-ext_modules = [
-    Extension(
-        'decimalfp._cdecimalfp',
-        ['src/decimalfp/_cdecimalfp.c'] + LIBFPDEC_SRC_FILES,
-        include_dirs=['src/decimalfp', LIBFPDEC_PATH],
-        extra_compile_args=extra_compile_args,
-        # extra_link_args="",
-        language='c',
-        ),
-    ]
+
+if have_gcc():
+    LIBFPDEC_PATH = 'src/decimalfp/libfpdec'
+    LIBFPDEC_SRC_FILES = sorted(f"{LIBFPDEC_PATH}/{fn}"
+                                for fn in os.listdir(path=LIBFPDEC_PATH)
+                                if fn.endswith(('.c',)))
+
+    DEBUG = int(os.getenv("DEBUG", 0))
+    cflags = sysconfig.get_config_var('CFLAGS')
+    if cflags:
+        extra_compile_args = cflags.split() + ["-Wall", "-Wextra"]
+    else:
+        extra_compile_args = ["-Wall", "-Wextra"]
+    if DEBUG:
+        extra_compile_args += ["-g3", "-O0", f"-DDEBUG={DEBUG}", "-UNDEBUG"]
+    else:
+        extra_compile_args += ["-DNDEBUG", "-O3"]
+
+    ext_modules = [
+        Extension(
+            'decimalfp._cdecimalfp',
+            ['src/decimalfp/_cdecimalfp.c'] + LIBFPDEC_SRC_FILES,
+            include_dirs=['src/decimalfp', LIBFPDEC_PATH],
+            extra_compile_args=extra_compile_args,
+            # extra_link_args="",
+            language='c',
+            ),
+        ]
+    package_data = {'decimalfp': ['py.typed', '_cdecimalfp.pyi']}
+else:
+    ext_modules = []
+    package_data = {}
 
 with open('README.md') as file:
     long_description = file.read()
@@ -46,7 +59,7 @@ setup(
     long_description_content_type="text/markdown",
     package_dir={'': 'src'},
     packages=['decimalfp'],
-    package_data={'decimalfp': ['py.typed', '_cdecimalfp.pyi']},
+    package_data=package_data,
     ext_modules=ext_modules,
     python_requires=">=3.7",
     license='BSD',
