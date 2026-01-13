463b531b4bc6f2a2a6f917601fe221df0842122f
Michael Amrhein <michael@adrhinum.de>
2026-01-13 13:38:35 +0100

    Updated license specifier; removed deprecated license classifyer; removed specific python versions.

--- setup.py
+++ setup.py
@@ -15,13 +15,15 @@ def have_gcc() -> bool:
 
 
 if have_gcc():
-    LIBFPDEC_PATH = 'src/decimalfp/libfpdec'
-    LIBFPDEC_SRC_FILES = sorted(f"{LIBFPDEC_PATH}/{fn}"
-                                for fn in os.listdir(path=LIBFPDEC_PATH)
-                                if fn.endswith(('.c',)))
+    LIBFPDEC_PATH = "src/decimalfp/libfpdec"
+    LIBFPDEC_SRC_FILES = sorted(
+        f"{LIBFPDEC_PATH}/{fn}"
+        for fn in os.listdir(path=LIBFPDEC_PATH)
+        if fn.endswith((".c",))
+    )
 
     DEBUG = int(os.getenv("DEBUG", 0))
-    cflags = sysconfig.get_config_var('CFLAGS')
+    cflags = sysconfig.get_config_var("CFLAGS")
     if cflags:
         extra_compile_args = cflags.split() + ["-Wall", "-Wextra"]
     else:
@@ -33,20 +35,20 @@ if have_gcc():
 
     ext_modules = [
         Extension(
-            'decimalfp._cdecimalfp',
-            ['src/decimalfp/_cdecimalfp.c'] + LIBFPDEC_SRC_FILES,
-            include_dirs=['src/decimalfp', LIBFPDEC_PATH],
+            "decimalfp._cdecimalfp",
+            ["src/decimalfp/_cdecimalfp.c"] + LIBFPDEC_SRC_FILES,
+            include_dirs=["src/decimalfp", LIBFPDEC_PATH],
             extra_compile_args=extra_compile_args,
             # extra_link_args="",
-            language='c',
-            ),
-        ]
-    package_data = {'decimalfp': ['py.typed', '_cdecimalfp.pyi']}
+            language="c",
+        ),
+    ]
+    package_data = {"decimalfp": ["py.typed", "_cdecimalfp.pyi"]}
 else:
     ext_modules = []
     package_data = {}
 
-with open('README.md') as file:
+with open("README.md") as file:
     long_description = file.read()
 
 setup(
@@ -57,30 +59,25 @@ setup(
     description="Decimal numbers with fixed-point arithmetic",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_dir={'': 'src'},
-    packages=['decimalfp'],
+    package_dir={"": "src"},
+    packages=["decimalfp"],
     package_data=package_data,
     ext_modules=ext_modules,
     python_requires=">=3.7",
-    license='BSD',
-    keywords='fixed-point decimal number datatype',
-    platforms='all',
+    license="BSD-2-Clause",
+    keywords="fixed-point decimal number datatype",
+    platforms="all",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-        ],
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     zip_safe=False,
-    )
+)
